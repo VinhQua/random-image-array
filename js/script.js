a37584786cae20f2671ee4b25f007d23a6442fc4
perry-platypus <dragonlinuxnoinochi@gmail.com>
2023-02-07 08:55:05 +0700

    Set up image api

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getRandomImage = async function(){
-    res = await fetch("https://picsum.photos/200");
-    console.log(res);
+const getRandomImages = async function(){
+    res = await fetch("https://picsum.photos/v2/list?page=2&limit=100");
+    images = res.json();
+    console.log(images);
 };
+getRandomImages();
